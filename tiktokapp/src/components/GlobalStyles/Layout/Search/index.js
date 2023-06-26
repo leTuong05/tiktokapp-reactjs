0aa38a5698ca3231e1cdd6df6d2238656671cfaa
leTuong05 <vantuongno1@gmail.com>
2023-06-26 11:01:49 +0700

    Logic header 3 (call Api use Axios)

--- tiktokapp/src/components/GlobalStyles/Layout/Search/index.js
+++ tiktokapp/src/components/GlobalStyles/Layout/Search/index.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
+import axios from 'axios';
 import classNames from "classnames/bind";
 import styles from './Search.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAfrica, faCircleQuestion, faKeyboard, faCloudUpload, faUser, faQuestion, faGear, faCoins, faSignOut } from '@fortawesome/free-solid-svg-icons';
 import { Wrapper as PopperWrapper } from '../../Popper';
 
+import request from '../../../../utils/request';
 import 'tippy.js/dist/tippy.css'; // optional
 import Tippy from '@tippyjs/react/headless';
 import AccountItem from '../../AccountItem';
+import useDebounce from '../../../hooks/useDebounce';
 
 const cx = classNames.bind(styles)
 
@@ -16,25 +19,40 @@ const Search = () => {
     const [searchValue, setSearchValue] = useState('')
     const [showResult, setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
+
+    const debounced = useDebounce(searchValue, 500)//sử dụng custom hook useDebounce  để tối ưu hoá, khi nhập xong input thì mới call API
+
     const refInput = useRef();
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setResultSearch([])
             return; // thoát khỏi hàm khi không có searchValue
         }
 
         setLoading(true)
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-            //encodeURIComponent dùng để mã hoá các ký tự ? & ...
-            .then(res => res.json())
-            .then(res => {
-                setResultSearch(res.data)
-                setLoading(false)
-            })
-            .catch(() => {
-                setLoading(false)
-            })
-    }, [searchValue])
+        request.get('users/search', { //sử dụng request để tối ưu hoá, sử dụng lại đc nhiều lần
+            params: {
+                q: debounced,
+                type: 'less'
+            }
+        }).then((res) => {
+            setResultSearch(res.data.data)
+            //console.log(res);
+            setLoading(false)
+        }).catch(() => {
+            setLoading(false)
+        })
+        // fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
+        //     //encodeURIComponent dùng để mã hoá các ký tự ? & ...
+        //     .then(res => res.json())
+        //     .then(res => {
+        //         setResultSearch(res.data)
+        //         setLoading(false)
+        //     })
+        //     .catch(() => {
+        //         setLoading(false)
+        //     })
+    }, [debounced])
 
     const handleHideResult = () => {
         setShowResult(false)
